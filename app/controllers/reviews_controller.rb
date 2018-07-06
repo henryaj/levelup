6571fc97f59a26551b25423a00160a7caea0264d
Henry Stanley <henry@henrystanley.com>
2018-07-06 14:18:25 -0400

    User can oAuth with GitHub

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -68,8 +68,8 @@ class ReviewsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_review
-      @review = Review.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_review
+    @review = Review.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
-
   resources :reviews
-
   root "pages#home"
 end
